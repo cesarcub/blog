d6c458300190268496a7d9037bb3613e3746c8ed
César Cubillos <cesarc.cubillos@gmail.com>
2020-03-01 12:51:10 -0500

    Se crean los modelos iniciales del blog

---
+++ src/Entity/Blog.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\BlogRepository")
+ */
+class Blog
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $titulo;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $contenido;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $fecha;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $imagen;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $idUsuaro;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitulo(): ?string
+    {
+        return $this->titulo;
+    }
+
+    public function setTitulo(string $titulo): self
+    {
+        $this->titulo = $titulo;
+
+        return $this;
+    }
+
+    public function getContenido(): ?string
+    {
+        return $this->contenido;
+    }
+
+    public function setContenido(string $contenido): self
+    {
+        $this->contenido = $contenido;
+
+        return $this;
+    }
+
+    public function getFecha(): ?\DateTimeInterface
+    {
+        return $this->fecha;
+    }
+
+    public function setFecha(\DateTimeInterface $fecha): self
+    {
+        $this->fecha = $fecha;
+
+        return $this;
+    }
+
+    public function getImagen(): ?string
+    {
+        return $this->imagen;
+    }
+
+    public function setImagen(string $imagen): self
+    {
+        $this->imagen = $imagen;
+
+        return $this;
+    }
+
+    public function getIdUsuaro(): ?int
+    {
+        return $this->idUsuaro;
+    }
+
+    public function setIdUsuaro(int $idUsuaro): self
+    {
+        $this->idUsuaro = $idUsuaro;
+
+        return $this;
+    }
+}
---
+++ src/Entity/Usuario.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\UsuarioRepository")
+ */
+class Usuario
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=60)
+     */
+    private $nombre;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $correo;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $clave;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $imagen;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNombre(): ?string
+    {
+        return $this->nombre;
+    }
+
+    public function setNombre(string $nombre): self
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    public function getCorreo(): ?string
+    {
+        return $this->correo;
+    }
+
+    public function setCorreo(string $correo): self
+    {
+        $this->correo = $correo;
+
+        return $this;
+    }
+
+    public function getClave(): ?string
+    {
+        return $this->clave;
+    }
+
+    public function setClave(string $clave): self
+    {
+        $this->clave = $clave;
+
+        return $this;
+    }
+
+    public function getImagen(): ?string
+    {
+        return $this->imagen;
+    }
+
+    public function setImagen(string $imagen): self
+    {
+        $this->imagen = $imagen;
+
+        return $this;
+    }
+}
